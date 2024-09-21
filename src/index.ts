568ceffb156e625900dacba33dd8d1a96cc142cb
Y姉 <akumakumu@gmail.com>
2024-09-22 03:58:56 +0700

    add typeorm

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
+import "reflect-metadata"
+
 import { logger } from './configs/logger.js';
+import express from 'express';
 
-const message: string = 'hello mom';
+const app = express();
 
-logger.info(message);
+const user = [
+    {id: 1, name: 'Aine', role: 'admin'},
+    {id: 1, name: 'Yui', role: 'user'},
+];
+
+const authorize = (...allowed_roles: string[]) => {
+    return (req: express.Request, res: express.Response, next: express.NextFunction) => {
+        // Todo Here
+    };
+};
+
