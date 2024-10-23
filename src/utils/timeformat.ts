6cca992c0bc8576b73ccb1748a51820bc630fe09
millimeter <akumakumu@gmail.com>
2024-10-23 20:32:59 +0700

    timestamp

--- src/utils/timeformat.ts
+++ src/utils/timeformat.ts
@@ -1,6 +1,8 @@
 export const formatDateTime = (date: Date): string => {
-    return date.toLocaleString('en-US', {
+    return date.toLocaleString('id-ID', {
         timeZone: 'Asia/Jakarta',
         hour12: false,
+        dateStyle: 'medium',
+        timeStyle: 'long',
     });
 };
